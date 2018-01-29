29c24674c2bc10d7e343608519756c0dd58a8f1f
Ruslan <sk8er_boi009@mail.ru>
2018-01-29 16:35:51 +0200

    Update DebitTest.scala

--- test/routers/DebitTest.scala
+++ test/routers/DebitTest.scala
@@ -13,35 +13,6 @@ class DebitTest extends BaseRouterSpecification {
 
   "Users Router" should {
 
-    //    "Not find the user" in new WithApplicationLoader(fakeAppLoader) {
-    //      val fakeRequest = FakeRequest(GET, "/users/123")
-    //      val Some(result) = route(fakeRequest)
-    //
-    //      status(result) must equalTo(NOT_FOUND)
-    //    }
-
-    //    "Create and Find the user" in new WithApplicationLoader(fakeAppLoader) {
-    //      val body = Json.toJson(User(87, "Gabriel"))
-    //      val fakePost = FakeRequest(POST, "/users").withJsonBody(body)
-    //      val Some(postResult) = route(fakePost)
-    //
-    //      status(postResult) must equalTo(CREATED)
-    //
-    //      val fakeRequest = FakeRequest(GET, "/users/87")
-    //      val Some(result) = route(fakeRequest)
-    //
-    //      status(result) must equalTo(OK)
-    //      contentType(result) must beSome("application/json")
-    //      contentAsJson(result) must equalTo(body)
-    //    }
-    //
-    //    "Have a specific handler to GET an User by id" in new WithApplication() {
-    //      val fakeRequest = FakeRequest(GET, "/users/87")
-    //      val handler = fakeRouter.handlerFor(fakeRequest)
-    //
-    //      handler must be_!=(None)
-    //    }
-
     "Have a specific handler to CREATE a Debit" in new WithApplication() {
       val body = DebitTransaction(11, 500, LocalDate.now())
       val fakeRequest = FakeRequest(POST, "/debit").withBody(body)
